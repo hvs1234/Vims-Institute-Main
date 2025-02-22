d05508230381b766ffa8fe17618db59d381d0342
hvs1234 <harshvardhansharma17@gmail.com>
2025-02-22 13:47:13 +0530

    add diploma course page

--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HiMenu } from "react-icons/hi";
 import Handlers from "../../Services/Handlers";
 import logo from "/Images/logo.png";
@@ -10,6 +10,7 @@ import { FaChevronDown } from "react-icons/fa";
 const Nav = () => {
   const { handleOnClick, toggleNavbar, isActive } = Handlers();
   const [dropdownOpen, setDropdownOpen] = useState(null);
+  const location = useLocation();
 
   const handleDropdownOpen = (id) => {
     setDropdownOpen(id);
@@ -27,9 +28,12 @@ const Nav = () => {
 
   useEffect(() => {
     document.addEventListener("click", closeDropdownOnClickOutside);
-    return () =>
-      document.removeEventListener("click", closeDropdownOnClickOutside);
+    return () => document.removeEventListener("click", closeDropdownOnClickOutside);
   }, []);
+
+  useEffect(() => {
+    toggleNavbar();
+  }, [location.pathname]);
 
   return (
     <div
@@ -38,16 +42,8 @@ const Nav = () => {
         isActive ? "active" : ""
       }`}
     >
-      <Link
-        to="/"
-        onClick={handleOnClick("/")}
-        className="hidden max-[1200px]:flex"
-      >
-        <img
-          src={logo}
-          alt="logo"
-          className="w-[40%] max-xl:w-[20%] max-md:w-[40%] max-[500px]:w-[50%]"
-        />
+      <Link to="/" onClick={handleOnClick("/")} className="hidden max-[1200px]:flex">
+        <img src={logo} alt="logo" className="w-[40%] max-xl:w-[20%] max-md:w-[40%] max-[500px]:w-[50%]" />
       </Link>
       <nav className="navbar flex items-center justify-center gap-[1rem] max-[1200px]:min-h-screen max-[1200px]:mt-[4rem]">
         <ul className="flex items-center gap-[4rem]">
@@ -70,7 +66,10 @@ const Nav = () => {
                         key={subItem.id}
                         to={subItem.to}
                         className="block px-4 py-2 text-[1.6rem] text-[#414141] hover:bg-gray-200"
-                        onClick={handleDropdownClose}
+                        onClick={() => {
+                          handleDropdownClose();
+                          toggleNavbar();
+                        }}
                       >
                         {subItem.name}
                       </Link>
@@ -82,9 +81,11 @@ const Nav = () => {
               <Link
                 key={item.id}
                 to={item.to || "#"}
-                onClick={
-                  item.to ? handleOnClick(item.to) : (e) => e.preventDefault()
-                }
+                onClick={(e) => {
+                  if (!item.to) e.preventDefault();
+                  handleOnClick(item.to);
+                  toggleNavbar();
+                }}
                 className="navlink text-[1.6rem] text-[#414141] font-normal uppercase transition-all duration-[0.2s] ease-linear hover:opacity-[0.8]"
               >
                 {item.name}
@@ -94,11 +95,7 @@ const Nav = () => {
         </ul>
       </nav>
       <div className="relative z-[9999]">
-        <HiMenu
-          size={20}
-          className="cursor-pointer nav-btn"
-          onClick={toggleNavbar}
-        />
+        <HiMenu size={20} className="cursor-pointer nav-btn" onClick={toggleNavbar} />
       </div>
     </div>
   );
